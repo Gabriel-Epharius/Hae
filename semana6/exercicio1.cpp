afd03a2dd44cc93a7229ed3f317a051b56745aaf
seu nome <thiago334@usp.br>
2021-06-23 20:15:58 -0300

    Oi

--- semana6/exercicio1.cpp
+++ semana6/exercicio1.cpp
@@ -1,1 +1,13 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2>
+#include <cekeikon.h>
+int main(int argc, char** argv) {
+    if (argc!=4) {
+    printf("cvvizinho ent.pgm sai.pgm fator\n");
+    erro("Erro: Numero de argumentos invalido");
+    }
+    Mat_<GRY> a; le(a,argv[1]);
+    double fator; sscanf(argv[3],"%lf",&fator);
+    Mat_<GRY> b;
+    resize(a, b, Size(0,0), fator, fator, INTER_NEAREST);
+    imp(b,argv[2]);
+}
